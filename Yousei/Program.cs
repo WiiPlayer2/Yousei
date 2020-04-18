4d7a20977351da833e2989d51da7a95c0630109a
Waldemar Tomme <waldetomme@gmail.com>
2020-04-18 11:30:18 +0200

    Initial commit

---
+++ Yousei/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+
+namespace Yousei
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseConsoleLifetime();
+    }
+}
