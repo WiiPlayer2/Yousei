dae2701ca66cb7495c09fce93caaf6fb061032f5
Waldemar Tomme <waldetomme@gmail.com>
2020-04-24 16:05:19 +0200

    Add debug information

--- Yousei/Modules/ArgumentsMapModule.cs
+++ Yousei/Modules/ArgumentsMapModule.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Reactive.Linq;
@@ -10,10 +11,12 @@ namespace Yousei.Modules
 {
     public class ArgumentsMapModule : IModule
     {
+        private readonly ILogger<ArgumentsMapModule> logger;
         private readonly ModuleRegistry moduleRegistry;
 
-        public ArgumentsMapModule(ModuleRegistry moduleRegistry)
+        public ArgumentsMapModule(ILogger<ArgumentsMapModule> logger, ModuleRegistry moduleRegistry)
         {
+            this.logger = logger;
             this.moduleRegistry = moduleRegistry;
         }
 
@@ -21,6 +24,7 @@ namespace Yousei.Modules
         {
             var jobAction = arguments.ToObject<JobAction>();
             var mappedArgs = jobAction.Arguments.Map(data);
+            logger.LogDebug($"Run module {jobAction.ModuleID} with mapped arguments {mappedArgs}");
             return moduleRegistry.GetModule(jobAction.ModuleID).MatchAsync(
                 module => module.ProcessAsync(mappedArgs, data, cancellationToken),
                 () => Observable.Throw<JToken>(new Exception($"Module {jobAction.ModuleID} not found.")));
