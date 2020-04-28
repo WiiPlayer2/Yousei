d41404ce6e30c6dd0d149c6b11a5b331490ae2e8
Waldemar Tomme <waldetomme@gmail.com>
2020-04-28 14:54:11 +0200

    Add WaitModule

---
+++ Yousei/WaitModule.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Yousei.Modules.Templates;
+
+namespace Yousei
+{
+    public class WaitModule : SingleTemplate
+    {
+        public override async Task<JToken> ProcessAsync(JToken arguments, JToken data, CancellationToken cancellationToken)
+        {
+            var intervalSeconds = arguments.ToObject<int>();
+            await Task.Delay(intervalSeconds * 1000, cancellationToken);
+            return data;
+        }
+    }
+}
