3b961122e55dde983b1f337d3ba33fb84d8ec860
Waldemar Tomme <waldetomme@gmail.com>
2020-04-28 19:52:10 +0200

    Allow multiple actions in loop module

--- Yousei/Modules/LoopModule.cs
+++ Yousei/Modules/LoopModule.cs
@@ -11,33 +11,32 @@ namespace Yousei.Modules
 {
     public class LoopModule : IModule
     {
+        private readonly JobFlowCreator jobFlowCreator;
+
         private class Arguments
         {
-            public string Until { get; set; }
+            public string Until { get; set; } = "$";
 
-            public JobAction Action { get; set; }
+            public List<JobAction> Actions { get; set; }
         }
 
-        private readonly ModuleRegistry moduleRegistry;
-
-        public LoopModule(ModuleRegistry moduleRegistry)
+        public LoopModule(JobFlowCreator jobFlowCreator)
         {
-            this.moduleRegistry = moduleRegistry;
+            this.jobFlowCreator = jobFlowCreator;
         }
 
-        public async Task<IObservable<JToken>> ProcessAsync(JToken arguments, JToken data, CancellationToken cancellationToken)
+        public Task<IObservable<JToken>> ProcessAsync(JToken arguments, JToken data, CancellationToken cancellationToken)
         {
             cancellationToken.ThrowIfCancellationRequested();
             var args = arguments.ToObject<Arguments>();
-
-            var moduleObservable = await moduleRegistry.RunAsync(args.Action.ModuleID, args.Action.Arguments, data, cancellationToken);
-            return moduleObservable.SelectMany(async (JToken prevData, CancellationToken token) =>
+            var flowObservable = jobFlowCreator.CreateJobFlow(args.Actions, data);
+            return flowObservable.SelectMany(async (JToken prevData, CancellationToken token) =>
             {
                 token.ThrowIfCancellationRequested();
                 if (prevData.Get(args.Until).ToObject<bool>())
                     return Observable.Return(data);
                 return await ProcessAsync(arguments, data, token);
-            }).SelectMany(o => o);
+            }).SelectMany(o => o).AsTask();
         }
     }
 }
