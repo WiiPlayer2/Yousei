7e5c4179db4d62ee0bd872f52ce2f280c6311a99
Waldemar Tomme <waldetomme@gmail.com>
2021-03-21 18:07:44 +0100

    Add TelegramConnector

---
+++ Yousei.Connectors/Telegram/TelegramConnection.cs
@@ -0,0 +1,15 @@
+﻿using Telegram.Bot;
+using Yousei.Core;
+
+namespace Yousei.Connectors.Telegram
+{
+    internal class TelegramConnection : SimpleConnection
+    {
+        private readonly TelegramBotClient telegramBotClient;
+
+        public TelegramConnection(TelegramBotClient telegramBotClient)
+        {
+            this.telegramBotClient = telegramBotClient;
+        }
+    }
+}
