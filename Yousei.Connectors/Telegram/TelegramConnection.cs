3735abc492fd220ece71b1c6daf85e1f67b23f7c
Waldemar Tomme <waldetomme@gmail.com>
2021-03-21 21:54:07 +0100

    Add telegram.onmessage & .onupdate

--- Yousei.Connectors/Telegram/TelegramConnection.cs
+++ Yousei.Connectors/Telegram/TelegramConnection.cs
@@ -1,4 +1,9 @@
-﻿using Telegram.Bot;
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
 using Yousei.Core;
 
 namespace Yousei.Connectors.Telegram
@@ -7,7 +12,34 @@ namespace Yousei.Connectors.Telegram
     {
         public TelegramConnection(TelegramBotClient telegramBotClient)
         {
+            var connectObservable = CreateConnectionObservable(telegramBotClient);
+            OnMessage = WrapEvent<MessageEventArgs>(connectObservable, telegramBotClient, nameof(TelegramBotClient.OnMessage));
+            OnUpdate = WrapEvent<UpdateEventArgs>(connectObservable, telegramBotClient, nameof(TelegramBotClient.OnUpdate));
+
+            AddTrigger("onmessage", new ObservableTrigger(OnMessage.Select(o => o.Message)));
+            AddTrigger("onupdate", new ObservableTrigger(OnUpdate.Select(o => o.Update)));
             AddAction("send", new SendAction(telegramBotClient));
+        }
+
+        public IObservable<MessageEventArgs> OnMessage { get; }
+
+        public IObservable<UpdateEventArgs> OnUpdate { get; }
+
+        private IObservable<Unit> CreateConnectionObservable(TelegramBotClient telegramBotClient)
+            => Observable.Create<Unit>(async (observer, cancellationToken) =>
+                {
+                    telegramBotClient.StartReceiving();
+                    cancellationToken.Register(telegramBotClient.StopReceiving);
+                    await Task.Delay(-1, cancellationToken);
+                })
+                .Publish()
+                .RefCount();
+
+        private IObservable<T> WrapEvent<T>(IObservable<Unit> connectionObservable, object target, string eventName)
+                    where T : class
+        {
+            var eventObservable = Observable.FromEventPattern<T>(target, eventName).Select(o => o.EventArgs);
+            return Observable.Merge(connectionObservable.Select(_ => default(T)), eventObservable);
         }
     }
 }
