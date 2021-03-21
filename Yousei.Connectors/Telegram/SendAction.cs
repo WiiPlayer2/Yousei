1d8785b80c3bb15ed994e06ca919b69f683842c8
Waldemar Tomme <waldetomme@gmail.com>
2021-03-21 22:28:18 +0100

    Add telegram connector (#4)
    * Remove type argument from SimpleConnection
    
    * Add SimpleConnector
    
    * Add TelegramConnector
    
    * Add string representations
    
    * Add telegram.send
    
    * Add telegram.onmessage & .onupdate
    
    * Rename send to sendtextmessage

---
+++ Yousei.Connectors/Telegram/SendAction.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Yousei.Core;
+using Yousei.Shared;
+
+namespace Yousei.Connectors.Telegram
+{
+    internal class SendAction : FlowAction<SendArguments>
+    {
+        private readonly TelegramBotClient telegramBotClient;
+
+        public SendAction(TelegramBotClient telegramBotClient)
+        {
+            this.telegramBotClient = telegramBotClient;
+        }
+
+        protected override async Task Act(IFlowContext context, SendArguments arguments)
+        {
+            var chatId = await arguments.ChatId.Resolve<ChatId>(context);
+            var text = await arguments.Text.Resolve<string>(context);
+
+            var message = await telegramBotClient.SendTextMessageAsync(chatId, text);
+            await context.SetData(message);
+        }
+    }
+}
