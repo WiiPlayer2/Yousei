c17c85f1ec8056d457fab5650b595cefa53000d6
Tim Nelke <tim.nelke@outlook.com>
2021-03-24 14:25:05 +0100

    Add IConfigurationDatabase

---
+++ Yousei.Shared/IConfigurationDatabase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yousei.Shared
+{
+    public interface IConfigurationDatabase
+    {
+        bool IsReadOnly { get; }
+
+        Task<IReadOnlyDictionary<string, IReadOnlyList<string>>> ListConfigurations();
+        Task<object> ReadConfiguration(string connector, string name);
+        Task SetConfiguration(string connector, string name, object configuration);
+
+        Task<IReadOnlyList<string>> ListFlows();
+        Task<FlowConfig> ReadFlow(string name);
+        Task SetFlow(string name, FlowConfig flowConfig);
+    }
+}
