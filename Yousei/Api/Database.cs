71d62a52d0d0373b78ca52481399515cef159250
Waldemar Tomme <waldetomme@gmail.com>
2021-03-26 10:16:15 +0100

    Move database related methods to Database

---
+++ Yousei/Api/Database.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Yousei.Api.Types;
+using Yousei.Shared;
+
+namespace Yousei.Api
+{
+    public class Database
+    {
+        private readonly IApi api;
+
+        public Database(IApi api)
+        {
+            this.api = api;
+        }
+
+        public bool IsReadOnly => api.ConfigurationDatabase.IsReadOnly;
+
+        public async Task<IEnumerable<Connection>> GetConnections()
+            => (await api.ConfigurationDatabase.ListConfigurations())
+                .Select(o => new Connection(o.Key, o.Value.Select(value => new Configuration(o.Key, value)).ToList()));
+
+        public async Task<IEnumerable<Flow>> GetFlows()
+            => (await api.ConfigurationDatabase.ListFlows())
+                .Select(o => new Flow(o));
+    }
+}
--- Yousei/Api/Query.cs
+++ Yousei/Api/Query.cs
@@ -9,21 +9,11 @@ namespace Yousei.Api
 {
     public class Query
     {
-        private readonly IApi api;
-
         public Query(IApi api)
         {
-            this.api = api;
+            Database = new Database(api);
         }
 
-        public bool IsReadOnly => api.ConfigurationDatabase.IsReadOnly;
-
-        public async Task<IEnumerable<Connection>> GetConnections()
-            => (await api.ConfigurationDatabase.ListConfigurations())
-                .Select(o => new Connection(o.Key, o.Value.Select(value => new Configuration(o.Key, value)).ToList()));
-
-        public async Task<IEnumerable<Flow>> GetFlows()
-            => (await api.ConfigurationDatabase.ListFlows())
-                .Select(o => new Flow(o));
+        public Database Database { get; }
     }
 }
