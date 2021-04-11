56af62ba4f43235b09097440bd4c99be1439e4fb
Waldemar Tomme <waldetomme@gmail.com>
2021-04-11 20:21:10 +0200

    Bug/fix UI cast error (#30)
    * Add GraphQL types
    
    * Use mapped types
    
    * Fix configurations not deletable

---
+++ Yousei.Web/Api/Types.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Yousei.Shared;
+
+namespace Yousei.Web.Api
+{
+    internal record Database(bool? IsReadOnly, Flow? Flow, Configuration? Configuration, Connection[]? Connections, Flow[]? Flows);
+
+    internal record Flow(string? Name, SourceConfig? Config);
+
+    internal record Configuration(string? Name, SourceConfig? Config);
+
+    internal record Connection(string? Id, Configuration[]? Configurations);
+
+    internal record Query(Database? Database);
+}
