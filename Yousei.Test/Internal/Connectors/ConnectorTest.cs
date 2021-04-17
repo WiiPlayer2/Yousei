c26bcb563d52f601a5df570809064e9ebac58e29
Waldemar Tomme <waldetomme@gmail.com>
2021-04-17 10:47:00 +0200

    Add LogConnectorTest

---
+++ Yousei.Test/Internal/Connectors/ConnectorTest.cs
@@ -0,0 +1,23 @@
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Yousei.Core;
+using Yousei.Shared;
+using YouseiRelaoded.Internal.Connectors.Log;
+
+namespace Yousei.Test.Internal.Connectors
+{
+    public abstract class ConnectorTest
+    {
+        protected readonly Mock<IFlowContext> flowContextMock = new();
+
+        protected Task Act(string name, object? arguments = default, object? configuration = default)
+            => CreateConnection(configuration)?.CreateAction(name)?.Act(flowContextMock.Object, arguments) ?? Task.CompletedTask;
+
+        protected abstract IConnection? CreateConnection(object? configuration = default);
+
+        protected abstract IConnector CreateConnector();
+    }
+}
---
+++ Yousei.Test/Internal/Connectors/LogConnectorTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Yousei.Core;
+using Yousei.Shared;
+using YouseiRelaoded.Internal.Connectors.Log;
+
+namespace Yousei.Test.Internal.Connectors
+{
+    [TestClass]
+    public class LogConnectorTest : ConnectorTest
+    {
+        private Mock<ILogger> loggerMock = new();
+
+        [TestMethod]
+        public async Task WriteActionWritesToLogger()
+        {
+            // Arrange
+            var arguments = new WriteArguments
+            {
+                Level = LogLevel.Information.ToConstantParameter(),
+                Tag = "test".ToConstantParameter(),
+                Message = "idek".ToConstantParameter(),
+            };
+
+            // Act
+            await Act("write", arguments);
+
+            // Assert
+            loggerMock.Verify(o => o.Log(
+                LogLevel.Information,
+                It.IsAny<EventId>(),
+                It.IsAny<It.IsAnyType>(),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception, string>>()));
+        }
+
+        protected override IConnection? CreateConnection(object? configuration = default)
+            => CreateConnector().GetConnection(configuration);
+
+        protected override IConnector CreateConnector()
+            => new LogConnector(loggerMock.Object);
+    }
+}
