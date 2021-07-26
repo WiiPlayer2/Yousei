a289edbaea62acb0bcf78ffc54be91fe8ae319f9
Waldemar Tomme <waldetomme@gmail.com>
2021-07-26 22:40:05 +0200

    Move triggers and actions to IConnector

--- Yousei.Shared/IConnection.cs
+++ Yousei.Shared/IConnection.cs
@@ -2,10 +2,8 @@
 
 namespace Yousei.Shared
 {
+    // Kept for possible extension purposes (and also to distinguish it from other IDisposables)
     public interface IConnection : IDisposable
     {
-        IFlowAction? CreateAction(string name);
-
-        IFlowTrigger? CreateTrigger(string name);
     }
 }
