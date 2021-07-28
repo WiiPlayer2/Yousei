160f4117fef5728f413c7c7ebed483f0edc2a233
Waldemar Tomme <waldetomme@gmail.com>
2021-07-28 19:01:14 +0200

    Add unit tests

--- Yousei.Core/MappedParameter.cs
+++ Yousei.Core/MappedParameter.cs
@@ -12,24 +12,24 @@ namespace Yousei.Core
 {
     internal class MappedParameter : IParameter
     {
-        private readonly IParameter baseParameter;
-
         private readonly Type targetType;
 
         public MappedParameter(IParameter baseParameter, Type targetType)
         {
-            this.baseParameter = baseParameter;
+            Base = baseParameter;
             this.targetType = targetType;
         }
 
+        public IParameter Base { get; }
+
         public async Task<object?> Resolve(IFlowContext context)
         {
-            var value = await baseParameter.Resolve(context);
+            var value = await Base.Resolve(context);
             return value.Map(targetType);
         }
 
         public override string ToString()
-            => baseParameter.ToString() ?? string.Empty;
+            => Base.ToString() ?? string.Empty;
     }
 
     internal class MappedParameter<T> : MappedParameter, IParameter<T>
