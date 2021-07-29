afbffa0a29c4f2854f6fdd2fac8ae7f72b4a6e55
Waldemar Tomme <waldetomme@gmail.com>
2021-07-29 18:31:12 +0200

    Allow multiple deletions

--- Yousei.Connectors/Imap/DeleteAction.cs
+++ Yousei.Connectors/Imap/DeleteAction.cs
@@ -1,23 +1,29 @@
 ﻿using MailKit;
 using MailKit.Net.Imap;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Yousei.Shared;
 
 namespace Yousei.Connectors.Imap
 {
+    internal record DeleteArguments
+    {
+        public string Folder { get; init; } = "INBOX";
+
+        public List<uint> IDs { get; init; } = new();
+    }
+
     internal class DeleteAction : ImapAction<DeleteArguments>
     {
         public override string Name { get; } = "delete";
 
         protected override async Task Act(IFlowContext context, ImapClient client, DeleteArguments arguments)
         {
-            var folderPath = await arguments.Folder.Resolve(context);
-            var id = await arguments.ID.Resolve(context);
-
-            var uniqueId = new UniqueId(id);
-            var folder = await client.GetFolderAsync(folderPath);
+            var ids = arguments.IDs.Select(o => new UniqueId(o)).ToList();
+            var folder = await client.GetFolderAsync(arguments.Folder);
             await folder.OpenAsync(FolderAccess.ReadWrite);
-            await folder.AddFlagsAsync(uniqueId, MessageFlags.Deleted, true);
+            await folder.AddFlagsAsync(ids, MessageFlags.Deleted, true);
             await folder.ExpungeAsync();
         }
     }
