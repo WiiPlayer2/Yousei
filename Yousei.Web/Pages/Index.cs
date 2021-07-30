499a4b7960942eebd1b6e2ae4d3ea37128f2dea3
Waldemar Tomme <waldetomme@gmail.com>
2021-07-30 12:20:57 +0200

    Replace Index with Index2

--- Yousei.Web/Pages/Index.cs
+++ Yousei.Web/Pages/Index.cs
@@ -11,7 +11,7 @@ using Yousei.Web.Model;
 
 namespace Yousei.Web.Pages
 {
-    public partial class Index2
+    public partial class Index
     {
         private Dictionary<string, List<string>> configurations = new();
 
@@ -32,7 +32,7 @@ namespace Yousei.Web.Pages
         public IJSRuntime Js { get; set; }
 
         [Inject]
-        public ILogger<Index2> Logger { get; set; }
+        public ILogger<Index> Logger { get; set; }
 
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
