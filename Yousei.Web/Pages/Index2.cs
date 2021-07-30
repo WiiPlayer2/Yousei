d8f0ee6cda29c8bdfa4c82b5899be8d6a8ae1425
Waldemar Tomme <waldetomme@gmail.com>
2021-07-30 07:28:27 +0200

    Fix nullable warnings

--- Yousei.Web/Pages/Index2.cs
+++ Yousei.Web/Pages/Index2.cs
@@ -13,6 +13,18 @@ namespace Yousei.Web.Pages
 {
     public partial class Index2
     {
+        private Dictionary<string, List<string>> configurations = new();
+
+        private ConfigModel? currentConfig = null;
+
+        private MonacoEditor? editor;
+
+        private List<string> flows = new();
+
+        private bool isReadOnly = true;
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
         [Inject]
         public IApi api { get; set; }
 
@@ -22,24 +34,7 @@ namespace Yousei.Web.Pages
         [Inject]
         public ILogger<Index2> logger { get; set; }
 
-        private List<string> flows = new();
-
-        private Dictionary<string, List<string>> configurations = new();
-
-        private MonacoEditor editor;
-
-        private ConfigModel currentConfig = null;
-
-        private bool isReadOnly = true;
-
-        private StandaloneEditorConstructionOptions Construct(MonacoEditor editor)
-            => new StandaloneEditorConstructionOptions
-            {
-                AutomaticLayout = true,
-                Language = "yaml",
-                Theme = "vs-dark",
-                ReadOnly = true,
-            };
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
         protected override async Task OnInitializedAsync()
         {
@@ -47,86 +42,6 @@ namespace Yousei.Web.Pages
             configurations = (await api.ConfigurationDatabase.ListConfigurations())
                 .ToDictionary(o => o.Key, o => o.Value.ToList());
             isReadOnly = await api.ConfigurationDatabase.IsReadOnly;
-        }
-
-        private async Task Save()
-        {
-            if (currentConfig is null || isReadOnly)
-                return;
-
-            try
-            {
-                var content = await editor.GetValue();
-                var model = await editor.GetModel();
-                //var language = await js.InvokeAsync<string>("blazorMonaco.editor.getModelLanguage", editor.Id, model.Id);
-                var language = "yaml";
-                await currentConfig.Save(new SourceConfig(language, content));
-            }
-            catch (Exception e)
-            {
-                logger.LogError(e, $"Exception while saving content.");
-            }
-        }
-
-        private async Task Delete()
-        {
-            if (currentConfig is null || isReadOnly)
-                return;
-
-            await currentConfig.Delete();
-            StateHasChanged();
-        }
-
-        private async Task SetConfig(ConfigModel configModel)
-        {
-            string content = default;
-            try
-            {
-                var sourceConfig = await configModel.Load();
-                content = sourceConfig.Content;
-                var model = await editor.GetModel();
-                await js.InvokeVoidAsync("blazorMonaco.editor.setModelLanguage", editor.Id, model.Id, sourceConfig.Language ?? string.Empty);
-            }
-            catch (Exception e)
-            {
-                logger.LogError(e, $"Exception while loading content.");
-            }
-
-            content ??= string.Empty;
-            await editor.SetValue(content);
-            currentConfig = configModel;
-
-            await editor.UpdateOptions(new()
-            {
-                ReadOnly = isReadOnly,
-            });
-        }
-
-        private async Task AddFlow()
-        {
-            if (isReadOnly)
-                return;
-
-            var name = await js.InvokeAsync<string>("prompt", "Enter flow name:", string.Empty);
-            if (string.IsNullOrWhiteSpace(name))
-                return;
-
-            flows.Add(name);
-            await SetConfig(new FlowConfigModel(name, api.ConfigurationDatabase));
-            this.StateHasChanged();
-        }
-
-        private async Task AddConnector()
-        {
-            if (isReadOnly)
-                return;
-
-            var name = await js.InvokeAsync<string>("prompt", "Enter connector name:", string.Empty);
-            if (string.IsNullOrWhiteSpace(name))
-                return;
-
-            configurations.Add(name, new List<string>());
-            this.StateHasChanged();
         }
 
         private async Task AddConfiguration(string connector)
@@ -143,6 +58,51 @@ namespace Yousei.Web.Pages
             this.StateHasChanged();
         }
 
+        private async Task AddConnector()
+        {
+            if (isReadOnly)
+                return;
+
+            var name = await js.InvokeAsync<string>("prompt", "Enter connector name:", string.Empty);
+            if (string.IsNullOrWhiteSpace(name))
+                return;
+
+            configurations.Add(name, new List<string>());
+            this.StateHasChanged();
+        }
+
+        private async Task AddFlow()
+        {
+            if (isReadOnly)
+                return;
+
+            var name = await js.InvokeAsync<string>("prompt", "Enter flow name:", string.Empty);
+            if (string.IsNullOrWhiteSpace(name))
+                return;
+
+            flows.Add(name);
+            await SetConfig(new FlowConfigModel(name, api.ConfigurationDatabase));
+            this.StateHasChanged();
+        }
+
+        private StandaloneEditorConstructionOptions Construct(MonacoEditor editor)
+            => new StandaloneEditorConstructionOptions
+            {
+                AutomaticLayout = true,
+                Language = "yaml",
+                Theme = "vs-dark",
+                ReadOnly = true,
+            };
+
+        private async Task Delete()
+        {
+            if (currentConfig is null || isReadOnly)
+                return;
+
+            await currentConfig.Delete();
+            StateHasChanged();
+        }
+
         private async Task Reload()
         {
             await api.Reload();
@@ -150,6 +110,53 @@ namespace Yousei.Web.Pages
             configurations = (await api.ConfigurationDatabase.ListConfigurations())
                 .ToDictionary(o => o.Key, o => o.Value.ToList());
             isReadOnly = await api.ConfigurationDatabase.IsReadOnly;
+        }
+
+        private async Task Save()
+        {
+            if (currentConfig is null || editor is null || isReadOnly)
+                return;
+
+            try
+            {
+                var content = await editor.GetValue();
+                var model = await editor.GetModel();
+                //var language = await js.InvokeAsync<string>("blazorMonaco.editor.getModelLanguage", editor.Id, model.Id);
+                var language = "yaml";
+                await currentConfig.Save(new SourceConfig(language, content));
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, $"Exception while saving content.");
+            }
+        }
+
+        private async Task SetConfig(ConfigModel configModel)
+        {
+            if (editor is null)
+                return;
+
+            string? content = default;
+            try
+            {
+                var sourceConfig = await configModel.Load();
+                content = sourceConfig?.Content ?? string.Empty;
+                var model = await editor.GetModel();
+                await js.InvokeVoidAsync("blazorMonaco.editor.setModelLanguage", editor.Id, model.Id, sourceConfig?.Language ?? string.Empty);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, $"Exception while loading content.");
+            }
+
+            content ??= string.Empty;
+            await editor.SetValue(content);
+            currentConfig = configModel;
+
+            await editor.UpdateOptions(new()
+            {
+                ReadOnly = isReadOnly,
+            });
         }
     }
 }
