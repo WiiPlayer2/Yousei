a61a477af6f5706eb696bdf5376ee30ec7467678
Waldemar Tomme <waldetomme@gmail.com>
2021-08-04 16:25:11 +0200

    Other stuff

---
+++ Yousei.Web/Shared/Editors/EditorBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Yousei.Web.Shared.Editors
+{
+    public abstract class EditorBase : ComponentBase
+    {
+        public abstract bool IsAllValid { get; }
+
+        public abstract bool IsValid { get; }
+
+        public abstract JToken BuildJToken();
+    }
+}
