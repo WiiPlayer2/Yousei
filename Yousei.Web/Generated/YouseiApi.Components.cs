6aa712f44924002e2e367c07b6676d6c567ed241
Waldemar Tomme <waldetomme@gmail.com>
2021-08-06 17:32:28 +0200

    Add VariableParameterEditor

--- Yousei.Web/Generated/YouseiApi.Components.cs
+++ Yousei.Web/Generated/YouseiApi.Components.cs
@@ -4,76 +4,6 @@
 namespace Yousei.Web.Api.Components
 {
     [global::System.CodeDom.Compiler.GeneratedCode("StrawberryShake", "12.0.0.0")]
-    public partial class LoadDataRenderer : global::StrawberryShake.Razor.QueryBase<global::Yousei.Web.Api.ILoadDataResult>
-    {
-        [global::Microsoft.AspNetCore.Components.InjectAttribute]
-        internal global::Yousei.Web.Api.LoadDataQuery Operation { get; set; } = default !;
-        protected override void OnInitialized()
-        {
-            Subscribe(Operation.Watch(strategy: Strategy));
-        }
-
-        protected override void OnParametersSet()
-        {
-            Subscribe(Operation.Watch(strategy: Strategy));
-        }
-    }
-
-    [global::System.CodeDom.Compiler.GeneratedCode("StrawberryShake", "12.0.0.0")]
-    public partial class GetConfigurationRenderer : global::StrawberryShake.Razor.QueryBase<global::Yousei.Web.Api.IGetConfigurationResult>
-    {
-        [global::Microsoft.AspNetCore.Components.InjectAttribute]
-        internal global::Yousei.Web.Api.GetConfigurationQuery Operation { get; set; } = default !;
-        [global::Microsoft.AspNetCore.Components.ParameterAttribute]
-        public global::System.String Connector { get; set; } = default !;
-        [global::Microsoft.AspNetCore.Components.ParameterAttribute]
-        public global::System.String Name { get; set; } = default !;
-        protected override void OnInitialized()
-        {
-            Subscribe(Operation.Watch(Connector, Name, strategy: Strategy));
-        }
-
-        protected override void OnParametersSet()
-        {
-            Subscribe(Operation.Watch(Connector, Name, strategy: Strategy));
-        }
-    }
-
-    [global::System.CodeDom.Compiler.GeneratedCode("StrawberryShake", "12.0.0.0")]
-    public partial class GetFlowRenderer : global::StrawberryShake.Razor.QueryBase<global::Yousei.Web.Api.IGetFlowResult>
-    {
-        [global::Microsoft.AspNetCore.Components.InjectAttribute]
-        internal global::Yousei.Web.Api.GetFlowQuery Operation { get; set; } = default !;
-        [global::Microsoft.AspNetCore.Components.ParameterAttribute]
-        public global::System.String FlowName { get; set; } = default !;
-        protected override void OnInitialized()
-        {
-            Subscribe(Operation.Watch(FlowName, strategy: Strategy));
-        }
-
-        protected override void OnParametersSet()
-        {
-            Subscribe(Operation.Watch(FlowName, strategy: Strategy));
-        }
-    }
-
-    [global::System.CodeDom.Compiler.GeneratedCode("StrawberryShake", "12.0.0.0")]
-    public partial class BlockConfigEditor_GetDataRenderer : global::StrawberryShake.Razor.QueryBase<global::Yousei.Web.Api.IBlockConfigEditor_GetDataResult>
-    {
-        [global::Microsoft.AspNetCore.Components.InjectAttribute]
-        internal global::Yousei.Web.Api.BlockConfigEditor_GetDataQuery Operation { get; set; } = default !;
-        protected override void OnInitialized()
-        {
-            Subscribe(Operation.Watch(strategy: Strategy));
-        }
-
-        protected override void OnParametersSet()
-        {
-            Subscribe(Operation.Watch(strategy: Strategy));
-        }
-    }
-
-    [global::System.CodeDom.Compiler.GeneratedCode("StrawberryShake", "12.0.0.0")]
     public partial class ListTypeEditor_GetDataRenderer : global::StrawberryShake.Razor.QueryBase<global::Yousei.Web.Api.IListTypeEditor_GetDataResult>
     {
         [global::Microsoft.AspNetCore.Components.InjectAttribute]
@@ -124,6 +54,76 @@ namespace Yousei.Web.Api.Components
         protected override void OnParametersSet()
         {
             Subscribe(Operation.Watch(TypeName, strategy: Strategy));
+        }
+    }
+
+    [global::System.CodeDom.Compiler.GeneratedCode("StrawberryShake", "12.0.0.0")]
+    public partial class BlockConfigEditor_GetDataRenderer : global::StrawberryShake.Razor.QueryBase<global::Yousei.Web.Api.IBlockConfigEditor_GetDataResult>
+    {
+        [global::Microsoft.AspNetCore.Components.InjectAttribute]
+        internal global::Yousei.Web.Api.BlockConfigEditor_GetDataQuery Operation { get; set; } = default !;
+        protected override void OnInitialized()
+        {
+            Subscribe(Operation.Watch(strategy: Strategy));
+        }
+
+        protected override void OnParametersSet()
+        {
+            Subscribe(Operation.Watch(strategy: Strategy));
+        }
+    }
+
+    [global::System.CodeDom.Compiler.GeneratedCode("StrawberryShake", "12.0.0.0")]
+    public partial class LoadDataRenderer : global::StrawberryShake.Razor.QueryBase<global::Yousei.Web.Api.ILoadDataResult>
+    {
+        [global::Microsoft.AspNetCore.Components.InjectAttribute]
+        internal global::Yousei.Web.Api.LoadDataQuery Operation { get; set; } = default !;
+        protected override void OnInitialized()
+        {
+            Subscribe(Operation.Watch(strategy: Strategy));
+        }
+
+        protected override void OnParametersSet()
+        {
+            Subscribe(Operation.Watch(strategy: Strategy));
+        }
+    }
+
+    [global::System.CodeDom.Compiler.GeneratedCode("StrawberryShake", "12.0.0.0")]
+    public partial class GetConfigurationRenderer : global::StrawberryShake.Razor.QueryBase<global::Yousei.Web.Api.IGetConfigurationResult>
+    {
+        [global::Microsoft.AspNetCore.Components.InjectAttribute]
+        internal global::Yousei.Web.Api.GetConfigurationQuery Operation { get; set; } = default !;
+        [global::Microsoft.AspNetCore.Components.ParameterAttribute]
+        public global::System.String Connector { get; set; } = default !;
+        [global::Microsoft.AspNetCore.Components.ParameterAttribute]
+        public global::System.String Name { get; set; } = default !;
+        protected override void OnInitialized()
+        {
+            Subscribe(Operation.Watch(Connector, Name, strategy: Strategy));
+        }
+
+        protected override void OnParametersSet()
+        {
+            Subscribe(Operation.Watch(Connector, Name, strategy: Strategy));
+        }
+    }
+
+    [global::System.CodeDom.Compiler.GeneratedCode("StrawberryShake", "12.0.0.0")]
+    public partial class GetFlowRenderer : global::StrawberryShake.Razor.QueryBase<global::Yousei.Web.Api.IGetFlowResult>
+    {
+        [global::Microsoft.AspNetCore.Components.InjectAttribute]
+        internal global::Yousei.Web.Api.GetFlowQuery Operation { get; set; } = default !;
+        [global::Microsoft.AspNetCore.Components.ParameterAttribute]
+        public global::System.String FlowName { get; set; } = default !;
+        protected override void OnInitialized()
+        {
+            Subscribe(Operation.Watch(FlowName, strategy: Strategy));
+        }
+
+        protected override void OnParametersSet()
+        {
+            Subscribe(Operation.Watch(FlowName, strategy: Strategy));
         }
     }
 }
